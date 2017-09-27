90cf85c2c44d1a26d48b079f02a3416a02f8e401
Dmitriy Krishtal <ntw.pride@gmail.com>
2017-09-27 22:40:05 +0300

    Cleans up

--- php/pay.php
+++ php/pay.php
@@ -1,13 +1,13 @@
 <?php
 
 $orderId = 12345;
-$payoutAddress = '0x39ee76948d238fad2b750998f8a38d80c73c7cd7'; //your address
+$payoutAddress = '0x39EE76948d238Fad2b750998F8A38d80c73c7Cd7'; //your address
 $callbackUrl = 'http://CHANGEME.com/callback.php?id='.$orderId;
 
 $url = sprintf('https://api.paybear.io/v1/eth/payment/%s/%s', $payoutAddress, urlencode($callbackUrl));
 if ($response = file_get_contents($url)) {
     $response = json_decode($response);
-    if (isset($response->success)) {
+    if (isset($response->data->address)) {
         echo $response->data->address;
         //save $data->data->invoice and keep it secret
     }
