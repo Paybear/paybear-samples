2991ddf9e0b59244028615cc835f8b930ea5fbc7
denigada <denigada@gmail.com>
2018-06-16 14:32:56 -0400

    update paybear.js
    missing code

--- nodejs/paybear.js
+++ nodejs/paybear.js
@@ -55,6 +55,7 @@ function getCurrency(token, orderId, getAddr, callback) {
       var currency = null;
 
       getCurrencies(function(currencies) {
+          token = token.toLowerCase();
           currency = currencies[token];
           currency['coinsValue'] = coinsValue;
 
