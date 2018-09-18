13a75d95e3f4cfa93f5880178b1979b759eca164
Indospace.io <justin@indospace.io>
2018-09-17 22:34:04 -0700

    fun cleanup

--- nodejs/status.js
+++ nodejs/status.js
@@ -2,20 +2,15 @@ const app = new (require('express').Router)();
 
 app.get('/paybear/status/:order', (req, res) => {
   var orderId = req.params.order;
-
 var confirmations = null;
-
+  
 confirmations = 0; //get from DB, see callback.php
 maxConfirmations = 3; //get from DB, see callback.php
-
 var resp = {
   success: confirmations >= maxConfirmations
 };
-
-if (confirmations!==null)
-  resp['confirmations'] = confirmations;
-
+if(confirmations !== null)
+  resp.confirmations = confirmations;
 res.json(resp); //return this data to PayBear form
 });
-
 module.exports = app;
